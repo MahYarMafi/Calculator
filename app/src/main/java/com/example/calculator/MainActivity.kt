cf38f7e9cd238722d6c73cb479298c98240f28d2
MahYar <mahyar.mafi@gmail.com>
2023-05-09 18:00:59 -0400

    Added subtraction using split and subString methods.

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -40,10 +40,40 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun onOperator(view: View){
+    fun onEqual(view: View) {
+        if (lastNumeric) {
+            var tvValue = tvInput?.text.toString()
+            var prefix = ""
+            try {
+                if (tvValue.startsWith("-")) {
+                    prefix = "-"
+                    tvValue = tvValue.substring(1)
+                }
+
+                if (tvValue.contains("-")){
+                    val splitValue = tvValue.split("-")
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+
+                    if (prefix.isNotEmpty()){
+                        one = prefix + one
+                    }
+
+                    tvInput?.text = (one.toDouble() - two.toDouble()).toString()
+                }
+
+
+
+            } catch (e: java.lang.ArithmeticException) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    fun onOperator(view: View) {
         tvInput?.text?.let {
 
-            if (lastNumeric && !isOperatorAdded(it.toString())){
+            if (lastNumeric && !isOperatorAdded(it.toString())) {
                 tvInput?.append((view as Button).text)
                 lastNumeric = false
                 lastDot = false
@@ -52,10 +82,10 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    private fun isOperatorAdded(value: String) : Boolean{
-        return if (value.startsWith("-")){
+    private fun isOperatorAdded(value: String): Boolean {
+        return if (value.startsWith("-")) {
             false
-        }else{
+        } else {
             value.contains("/") || value.contains("*")
                     || value.contains("+") || value.contains("-")
         }
