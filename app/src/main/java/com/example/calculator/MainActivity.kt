5aef51555d80d9ebdeec60227c4c71f45fc86c4a
MahYar <mahyar.mafi@gmail.com>
2023-05-09 10:59:00 -0400

    DecimalPoint functionality added.

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -11,6 +11,10 @@ import android.widget.Toast
 class MainActivity : AppCompatActivity() {
     private var tvInput: TextView? = null
 
+    var lastNumeric: Boolean = false
+    var lastDot: Boolean = false
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -18,12 +22,21 @@ class MainActivity : AppCompatActivity() {
         tvInput = findViewById(R.id.tvInput)
     }
 
-    fun onDigit(view: View){
-       val num = (view as Button).text
-        tvInput?.append(num.toString())
+    fun onDigit(view: View) {
+        tvInput?.append((view as Button).text)
+        lastNumeric = true
+
     }
 
-fun onClear(view: View){
-    tvInput?.text = ""
-}
+    fun onClear(view: View) {
+        tvInput?.text = ""
+    }
+
+    fun onDecimalPoint(view: View) {
+        if (lastNumeric && !lastDot) {
+            tvInput?.append(".")
+            lastNumeric = false
+            lastDot = true
+        }
+    }
 }
